2e49ea544efff048dc3e994c7270b0444a71c048
= <=>
2020-12-20 17:20:26 -0600

    Exporta los estilos

--- lib/video_editor.dart
+++ lib/video_editor.dart
@@ -1,5 +1,6 @@
 library video_editor;
 
+export 'package:video_editor/utils/styles.dart';
 export 'package:video_editor/utils/controller.dart';
 export 'package:video_editor/widgets/crop/crop_grid.dart';
 export 'package:video_editor/widgets/trim/trim_slider.dart';
