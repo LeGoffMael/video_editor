be52304090abb7b0a4871255199a354191d98ed7
= <=>
2020-12-22 14:03:20 -0600

    Se escala el crop cuando se cambia de rotacion

--- lib/widgets/crop/crop_grid.dart
+++ lib/widgets/crop/crop_grid.dart
@@ -38,6 +38,7 @@ class _CropGridViewState extends State<CropGridView> {
   double _scale = 1.0;
   Size _layout = Size.zero;
   Rect _rect;
+  Orientation _orientation;
 
   @override
   void initState() {
@@ -49,20 +50,7 @@ class _CropGridViewState extends State<CropGridView> {
   void didUpdateWidget(CropGridView oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (!widget.showGrid && !widget.controller.isPlaying)
-      setState(() {
-        _rect = _calculateCropRect();
-        final double _scaleX = _layout.width / _rect.width;
-        final double _scaleY = _layout.height / _rect.height;
-        if (_aspect < 1.0)
-          _scale = _scaleX > _scaleY ? _scaleY : _scaleX;
-        else
-          _scale = _scaleX < _scaleY ? _scaleY : _scaleX;
-        _translate = Offset(
-              (_layout.width - _rect.width) / 2,
-              (_layout.height - _rect.height) / 2,
-            ) -
-            _rect.topLeft;
-      });
+      setState(() => _scaleRect());
   }
 
   void onPanStart(details) {
@@ -135,12 +123,18 @@ class _CropGridViewState extends State<CropGridView> {
       final Offset delta = details.delta;
       switch (boundary) {
         case CropBoundaries.topLeft:
-          final pos = _rect.topLeft + delta;
+          final Offset pos = _rect.topLeft + delta;
           _changeRect(
             top: pos.dy,
             left: pos.dx,
             width: _rect.width - delta.dx,
             height: _rect.height - delta.dy,
+          );
+          break;
+        case CropBoundaries.bottomRight:
+          _changeRect(
+            width: _rect.width + delta.dx,
+            height: _rect.height + delta.dy,
           );
           break;
         case CropBoundaries.topRight:
@@ -154,12 +148,6 @@ class _CropGridViewState extends State<CropGridView> {
           _changeRect(
             left: _rect.bottomLeft.dx + delta.dx,
             width: _rect.width - delta.dx,
-            height: _rect.height + delta.dy,
-          );
-          break;
-        case CropBoundaries.bottomRight:
-          _changeRect(
-            width: _rect.width + delta.dx,
             height: _rect.height + delta.dy,
           );
           break;
@@ -182,7 +170,7 @@ class _CropGridViewState extends State<CropGridView> {
           _changeRect(width: _rect.width + delta.dx);
           break;
         case CropBoundaries.inside:
-          final pos = _rect.topLeft + delta;
+          final Offset pos = _rect.topLeft + delta;
           _changeRect(left: pos.dx, top: pos.dy);
           break;
       }
@@ -225,28 +213,47 @@ class _CropGridViewState extends State<CropGridView> {
     );
   }
 
+  void _scaleRect() {
+    _rect = _calculateCropRect();
+    final double _scaleX = _layout.width / _rect.width;
+    final double _scaleY = _layout.height / _rect.height;
+    if (_aspect < 1.0)
+      _scale = _scaleX > _scaleY ? _scaleY : _scaleX;
+    else
+      _scale = _scaleX < _scaleY ? _scaleY : _scaleX;
+    _translate = Offset(
+          (_layout.width - _rect.width) / 2,
+          (_layout.height - _rect.height) / 2,
+        ) -
+        _rect.topLeft;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Transform.scale(
       scale: _scale,
       child: Transform.translate(
         offset: _translate,
-        child: VideoViewer(
-          controller: widget.controller,
-          child: LayoutBuilder(builder: (_, constraints) {
-            _layout = Size(constraints.maxWidth, constraints.maxHeight);
-            if (_rect == null) _rect = _calculateCropRect();
-
-            return widget.showGrid
-                ? GestureDetector(
-                    onPanUpdate: onPanUpdate,
-                    onPanStart: onPanStart,
-                    onPanEnd: _onPanEnd,
-                    child: _paint(),
-                  )
-                : _paint();
-          }),
-        ),
+        child: OrientationBuilder(builder: (_, orientation) {
+          return VideoViewer(
+            controller: widget.controller,
+            child: LayoutBuilder(builder: (_, constraints) {
+              _layout = Size(constraints.maxWidth, constraints.maxHeight);
+              if (_orientation != orientation) {
+                _orientation = orientation;
+                _scaleRect();
+              }
+              return widget.showGrid
+                  ? GestureDetector(
+                      onPanUpdate: onPanUpdate,
+                      onPanStart: onPanStart,
+                      onPanEnd: _onPanEnd,
+                      child: _paint(),
+                    )
+                  : _paint();
+            }),
+          );
+        }),
       ),
     );
   }
