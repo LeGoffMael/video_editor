edae839aaa510dd7f2c947a89ba5a8dd4375d393
= <=>
2020-12-23 15:43:44 -0600

    Se privatizo el _TrimBoundaries t se refactorizo codigo

--- lib/widgets/trim/trim_slider.dart
+++ lib/widgets/trim/trim_slider.dart
@@ -4,7 +4,7 @@ import 'package:video_editor/widgets/trim/trim_slider_painter.dart';
 import 'package:video_editor/widgets/trim/thumbnail_slider.dart';
 import 'package:video_player/video_player.dart';
 
-enum TrimBoundaries { left, right, inside, progress }
+enum _TrimBoundaries { left, right, inside, progress }
 
 class TrimSlider extends StatefulWidget {
   TrimSlider({
@@ -24,7 +24,7 @@ class TrimSlider extends StatefulWidget {
 
 class _TrimSliderState extends State<TrimSlider> {
   VideoPlayerController _controller;
-  TrimBoundaries boundary;
+  _TrimBoundaries boundary;
   double _progressTrim = 0.0;
   Size _layout = Size.zero;
   Rect _rect;
@@ -54,20 +54,22 @@ class _TrimSliderState extends State<TrimSlider> {
     final double pos = details.localPosition.dx;
     final double max = _rect.right;
     final double min = _rect.left;
+    final List<double> minMargin = [min - margin, min + margin];
+    final List<double> maxMargin = [max - margin, max + margin];
 
     //IS TOUCHING THE GRID
-    if (pos >= min - margin && pos <= max + margin) {
+    if (pos >= minMargin[0] && pos <= maxMargin[1]) {
       //TOUCH BOUNDARIES
-      if (pos >= min - margin && pos <= min + margin) {
-        boundary = TrimBoundaries.left;
-      } else if (pos >= max - margin && pos <= max + margin) {
-        boundary = TrimBoundaries.right;
-      } else if (pos >= _progressTrim - margin &&
-          pos <= _progressTrim + margin) {
-        boundary = TrimBoundaries.progress;
-      } else {
-        boundary = TrimBoundaries.inside;
-      }
+      if (pos >= minMargin[0] && pos <= minMargin[1])
+        boundary = _TrimBoundaries.left;
+      else if (pos >= maxMargin[0] && pos <= maxMargin[1])
+        boundary = _TrimBoundaries.right;
+      else if (pos >= _progressTrim - margin && pos <= _progressTrim + margin)
+        boundary = _TrimBoundaries.progress;
+      else if (pos >= minMargin[1] && pos <= maxMargin[0])
+        boundary = _TrimBoundaries.inside;
+      else
+        boundary = null;
     } else {
       boundary = null;
     }
@@ -78,18 +80,18 @@ class _TrimSliderState extends State<TrimSlider> {
     if (boundary != null) {
       final Offset delta = details.delta;
       switch (boundary) {
-        case TrimBoundaries.left:
+        case _TrimBoundaries.left:
           final pos = _rect.topLeft + delta;
           _changeRect(left: pos.dx, width: _rect.width - delta.dx);
           break;
-        case TrimBoundaries.right:
+        case _TrimBoundaries.right:
           _changeRect(width: _rect.width + delta.dx);
           break;
-        case TrimBoundaries.inside:
+        case _TrimBoundaries.inside:
           final pos = _rect.topLeft + delta;
           _changeRect(left: pos.dx);
           break;
-        case TrimBoundaries.progress:
+        case _TrimBoundaries.progress:
           final double pos = details.localPosition.dx;
           if (pos >= _rect.left && pos <= _rect.right) {
             if (_controller.value.isPlaying) {
@@ -107,7 +109,7 @@ class _TrimSliderState extends State<TrimSlider> {
   void onHorizontalDragEnd(_) async {
     final double width = _layout.width;
     widget.controller.updateTrim(_rect.left / width, _rect.right / width);
-    if (boundary == TrimBoundaries.progress) {
+    if (boundary == _TrimBoundaries.progress) {
       await _controller.seekTo(
         _controller.value.duration * (_progressTrim / width),
       );
