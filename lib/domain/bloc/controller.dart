aaa28408bd2700f4e422fc06b24b04169eb15f9a
Alexandr Smirnov <32673518+AlexSmirnov9107@users.noreply.github.com>
2022-02-22 16:10:37 +0600

    Update controller.dart
    isFiltersEnabled

--- lib/domain/bloc/controller.dart
+++ lib/domain/bloc/controller.dart
@@ -407,6 +407,7 @@ class VideoEditorController extends ChangeNotifier {
     String? customInstruction,
     void Function(Statistics)? onProgress,
     VideoExportPreset preset = VideoExportPreset.none,
+    isFiltersEnabled = true,
   }) async {
     final String tempPath = outDir ?? (await getTemporaryDirectory()).path;
     final String videoPath = file.path;
@@ -434,7 +435,7 @@ class VideoEditorController extends ChangeNotifier {
     final List<String> filters = [crop, scaleInstruction, rotation, gif];
     filters.removeWhere((item) => item.isEmpty);
     final String filter =
-        filters.isNotEmpty ? "-filter:v " + filters.join(",") : "";
+        filters.isNotEmpty && isFiltersEnabled ? "-filter:v " + filters.join(",") : "";
     final String execute =
         " -i \'$videoPath\' ${customInstruction ?? ""} $filter ${_getPreset(preset)} $trim -y $outputPath";
 
