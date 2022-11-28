816c1a72eecef5150c8e3d5cdde860232fe6b76c
Maël Le Goff <legoffmael@gmail.com>
2022-11-28 14:51:45 +0900

    [example] update example crop screen

--- example/lib/crop.dart
+++ example/lib/crop.dart
@@ -1,7 +1,6 @@
-import 'package:example/widgets/icon_text_button.dart';
 import 'package:flutter/material.dart';
-import 'package:video_editor/domain/bloc/controller.dart';
-import 'package:video_editor/ui/crop/crop_grid.dart';
+import 'package:fraction/fraction.dart';
+import 'package:video_editor/video_editor.dart';
 
 class CropScreen extends StatelessWidget {
   const CropScreen({super.key, required this.controller});
@@ -40,7 +39,7 @@ class CropScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 15),
-            Row(children: [
+            Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
               Expanded(
                 flex: 2,
                 child: IconButton(
@@ -53,39 +52,75 @@ class CropScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              IconTextButton(
-                title: "16:9",
-                onTap: () => controller.preferredCropAspectRatio = 16 / 9,
-              ),
-              IconTextButton(
-                title: "1:1",
-                onTap: () => controller.preferredCropAspectRatio = 1,
-              ),
-              IconTextButton(
-                title: "9:16",
-                onTap: () => controller.preferredCropAspectRatio = 9 / 16,
-              ),
-              IconTextButton(
-                title: "free",
-                onTap: () => controller.preferredCropAspectRatio = null,
+              Expanded(
+                flex: 4,
+                child: AnimatedBuilder(
+                  animation: controller,
+                  builder: (_, __) => Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () =>
+                                controller.preferredCropAspectRatio = controller
+                                    .preferredCropAspectRatio
+                                    ?.toFraction()
+                                    .inverse()
+                                    .toDouble(),
+                            icon: controller.preferredCropAspectRatio != null &&
+                                    controller.preferredCropAspectRatio! < 1
+                                ? const Icon(
+                                    Icons.panorama_vertical_select_rounded)
+                                : const Icon(Icons.panorama_vertical_rounded),
+                          ),
+                          IconButton(
+                            onPressed: () =>
+                                controller.preferredCropAspectRatio = controller
+                                    .preferredCropAspectRatio
+                                    ?.toFraction()
+                                    .inverse()
+                                    .toDouble(),
+                            icon: controller.preferredCropAspectRatio != null &&
+                                    controller.preferredCropAspectRatio! > 1
+                                ? const Icon(
+                                    Icons.panorama_horizontal_select_rounded)
+                                : const Icon(Icons.panorama_horizontal_rounded),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          _buildCropButton(context, null),
+                          _buildCropButton(context, 1.toFraction()),
+                          _buildCropButton(
+                              context, Fraction.fromString("9/16")),
+                          _buildCropButton(context, Fraction.fromString("3/4")),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ),
               Expanded(
                 flex: 2,
                 child: IconButton(
                   onPressed: () {
-                    //2 WAYS TO UPDATE CROP
-                    //WAY 1:
+                    // 2 WAYS TO UPDATE CROP
+                    // WAY 1:
                     controller.updateCrop();
-                    /*WAY 2:
-                    controller.minCrop = controller.cacheMinCrop;
-                    controller.maxCrop = controller.cacheMaxCrop;
-                    */
+                    // WAY 2:
+                    // controller.minCrop = controller.cacheMinCrop;
+                    // controller.maxCrop = controller.cacheMaxCrop;
                     Navigator.pop(context);
                   },
-                  icon: const Center(
+                  icon: Center(
                     child: Text(
                       "done",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                        color: const CropGridStyle().selectedBoundariesColor,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
@@ -93,6 +128,28 @@ class CropScreen extends StatelessWidget {
             ]),
           ]),
         ),
+      ),
+    );
+  }
+
+  Widget _buildCropButton(BuildContext context, Fraction? f) {
+    if (controller.preferredCropAspectRatio != null &&
+        controller.preferredCropAspectRatio! > 1) f = f?.inverse();
+
+    return Flexible(
+      child: TextButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: controller.preferredCropAspectRatio == f?.toDouble()
+              ? Colors.grey.shade800
+              : null,
+          foregroundColor: controller.preferredCropAspectRatio == f?.toDouble()
+              ? Colors.white
+              : null,
+          textStyle: Theme.of(context).textTheme.bodySmall,
+        ),
+        onPressed: () => controller.preferredCropAspectRatio = f?.toDouble(),
+        child: Text(f == null ? 'free' : '${f.numerator}:${f.denominator}'),
       ),
     );
   }
